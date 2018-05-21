dd4433228789a1ff175400f94b92c1c049948ed1
NiceToHaveThings <ben.marten@gmx.de>
2018-05-21 20:43:16 +0200

    Utils Require

---
+++ NiceToHave.Utils/Require.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NiceToHave.Utils
+{
+    public static class Require
+    {
+        public static void NotNull(object element, string message)
+        {
+            if(element == null)
+            {
+                throw new ArgumentException(message);
+            }
+        }
+    }
+}
